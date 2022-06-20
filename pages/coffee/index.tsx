a84846e2cd8f5eaf53c337cdc895c32538570c57
정민킴 <jma1020@naver.com>
2022-06-20 23:19:15 +0900

    ⚡️ type 수정 및  styled 적용

--- pages/coffee/index.tsx
+++ pages/coffee/index.tsx
@@ -1,11 +1,18 @@
 import React, { useEffect } from "react"
 import axios from 'axios'
 import { GetStaticPaths, GetStaticProps } from "next"
+import Link from "next/link"
+import { MEDIA_QUERY_ENDPOINT } from "../../shared/constants"
+import type { NextPage } from 'next'
+import styled from "@emotion/styled"
+
 
 interface Props {
-  data: MovieData
-  status: string,
-  status_message: string
+  data:{
+    data: MovieData
+    status: string,
+    status_message: string
+  }
 }
 
 interface MovieData {
@@ -25,34 +32,73 @@ interface Movie {
   year: number,
   summary: string
 }
-// interface 
 
 
- function CoffeePage({data} :Props) {
+const CoffeePage: NextPage<Props> =({data})=> {
   console.log(data)
 
-
   return (
-    <div>
-      {data.data.movies.map((item: Movie,index: number)=>{
-        return(
-          <h1 key={index}>{item.title}</h1>
+    <Container as="section">
+      {data.data.movies.map((item: Movie,index: number)=>
+        (
+          <Link href={`/coffee/${item.id.toString()}`}  passHref  key={`${item.id}`}>
+            <CardLink>{item.title}</CardLink>
+          </Link>
         )
-      })}
-    </div>
+    )}
+    </Container>
   )
 }
 
-export const getStaticProps: GetStaticProps = async  (context) => {
+export const getStaticProps: GetStaticProps = async (context) => {
   const {data} = await axios.get(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
-
+  
   return {
     props: {
-      data,
+      data : data,
     }
   }
 }
 
 
+
+
+// export async function getStaticProps(context){
+//   const {data} = await axios.get(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
+
+//   return {
+//     props: {
+//       data,
+//     }
+//   }
+// }
+
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  padding: 100px;
+  gap: 60px;
+  @media (max-width: ${MEDIA_QUERY_ENDPOINT.TABLE}){
+    grid-template-columns: repeat(3,1fr);
+  }
+
+  @media (max-width: ${MEDIA_QUERY_ENDPOINT.MOBILE}) {
+    grid-template-columns: repeat(2,1fr);
+  }
+`
+
+const CardLink = styled.a`
+  max-width: 360px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; 
+  box-sizing: border-box;
+  &:hover {
+    border: 10px solid white;
+    border-radius: 20px;
+  
+  }
+`
 
 export default CoffeePage
